ecdfa343b492e23cabe67cdd68b223b6dd391aa8
André Cerutti Franciscatto <andre@franciscatto.com>
2021-08-24 15:20:38 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+def sendmailalternative(sender, password, receiver, title, message):
+    try:
+        msg = EmailMessage()
+        msg.set_content(message)
+
+        msg['Subject'] = title
+        msg['From'] = sender
+        msg['To'] = receiver
+
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        sleep(3)
+        server.login(sender, password)
+        sleep(3)
+        server.send_message(msg)
+        server.quit()
+        return "Email sent"
+    except Exception as err:
+        return err
